60ef60ed9d0f3a6c104fc965f9a4c0c27049e1e6
Andrew Janosik <andrew.janosik@gmail.com>
2022-10-05 02:07:33 -0500

    Completed associations with one to many and ect

--- 02-Challenge/Develop/models/index.js
+++ 02-Challenge/Develop/models/index.js
@@ -5,13 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category,{
+  foreignKey:'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product,{
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+ProductTag.belongToMany(Tag,{
+  foreignKey: 'tag_id',
+});
 // Tags belongToMany Products (through ProductTag)
-
+ProductTag.belongToMany(Product,{
+  foreignKey: 'product_id',
+});
 module.exports = {
   Product,
   Category,
